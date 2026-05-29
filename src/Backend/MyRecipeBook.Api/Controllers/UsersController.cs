2680e18f1172efad989dc2520809a32bb3bc4b8e
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2026-05-29 15:48:50 -0300

    feature/ Implementando o endpoint Register com UsersController

---
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRecipeBook.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+}
