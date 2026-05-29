8a2c391e55601d3afdab3f8e56af9edd96433821
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2026-05-29 15:57:07 -0300

    fix/Resolvendo bugs de commits e problemas de pastas no VS

--- src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
 
 namespace MyRecipeBook.Api.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : ControllerBase
 {
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterUserAccountJson request)
+    {
+        return Created();
+    }
 }
