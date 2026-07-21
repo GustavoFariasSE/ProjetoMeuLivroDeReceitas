31e057db9be6b5b372cd9325bcff4fa574d94095
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2026-07-21 09:47:30 -0300

    feature/Creating unit tests for the validators using the AAA pattern, and testing: if the name is empty, if the email is empty, if the password is empty, and finally, if the email is invalid.

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -13,7 +13,7 @@ public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUse
         RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED);
         When(user => user.Email.IsNotEmpty(), () =>
         {
-            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.VALIDATION_EMAIL_INVALID);
         });
     }
 }
---
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exception;
+using Shouldly;
+
+namespace Validators.Tests.User.Register;
+public class RegisterUserAccountValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var validator = new RegisterUserAccountValidator(); 
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+
+        var result = validator.Validate(request);
+
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(errors => errors.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+
+        var result = validator.Validate(request);
+
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(errors => errors.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenPasswordIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+
+        var result = validator.Validate(request);
+
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(errors => errors.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsInEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = "we.com";
+
+        var validator = new RegisterUserAccountValidator();
+
+
+        var result = validator.Validate(request);
+
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(errors => errors.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
+    }
+}
