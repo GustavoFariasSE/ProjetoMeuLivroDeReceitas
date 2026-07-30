054b9b45a17a496b7f76e8a5713755de73e22bb4
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2026-07-30 08:51:28 -0300

    feature/ When creating unit tests for the use case—following the folder structure and the `RegisterUserAccountUseCase` class naming convention—mocks were used to create fake objects for each interface.

---
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,36 @@
+﻿using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Security;
+using MyRecipeBook.Application.UseCases.User.Register;
+using Shouldly;
+
+namespace UseCases.Tests.User.Register;
+public class RegisterUserAccountUseCaseTests
+{
+    [Fact]
+    public async Task Sucess()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.TokensJson.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+        result.TokensJson.AccessToken.ShouldBeNullOrEmpty();
+        result.TokensJson.RefreshToken.ShouldBeNullOrEmpty();
+    }
+
+    private RegisterUserAccountUseCase CreateUseCase()
+    {
+        var unitOfWork = IUnityOfWorkBuilder.Build();
+        var userWriteOnlyRepository = IUserWriteOnlyRepositoryBuilder.Build();
+        var userReadOnlyRepository = new IUserReadOnlyRepositoryBuilder().Build();
+        var passwordHasher = new IPasswordHasherBuilder().Build();
+
+        return new RegisterUserAccountUseCase(passwordHasher, userWriteOnlyRepository, 
+            unitOfWork, userReadOnlyRepository);
+    }
+}
